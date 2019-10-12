266d391595c67775b5c1d3dfcd13678703007bc9
Marius Tolzmann <m@rius.berlin>
2019-10-12 20:32:28 +0200

    use FindLeafFiles()

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
-import "github.com/mineiros-io/terradude/dude"
+import (
+	"os"
+	"log"
+	"github.com/mineiros-io/terradude/config"
+	"github.com/mineiros-io/terradude/util"
+	"github.com/mineiros-io/terradude/dude"
+)
+
 
 // This variable is set at build time using -ldflags parameters. For more info, see:
 // http://stackoverflow.com/a/11355611/483528
 var VERSION string
 
 func main() {
-	dude.RunFmt()
+	leafs, _ := util.FindLeafFiles(config.DefaultConfigPath, os.Args[1:], nil)
+
+	for _, leaf := range leafs {
+		log.Printf("start processing %v\n", leaf)
+		dude.RunFmt(leaf)
+		log.Printf("finished processing %v\n", leaf)
+	}
 }
