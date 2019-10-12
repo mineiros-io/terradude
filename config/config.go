97c7cb364be079ae4033271a74208b2abd208b99
Marius Tolzmann <m@rius.berlin>
2019-10-12 22:32:12 +0200

    next iteration of config structure
    - support multiple native providers
    - support multiple dependencies
    - unsupport define/template for now as not needed

--- config/config.go
+++ config/config.go
@@ -8,18 +8,20 @@ const DefaultConfigPath = "terradude.hcl"
 
 // represents a parsed config
 type Config struct {
-	Terradude  *Terradude  `hcl:"terradude,block"`
-	Terraform  *Terraform  `hcl:"terraform,block"`
-	Dependency *Dependency `hcl:"dependency,block"`
-	Backend    *Backend    `hcl:"backend,block"`
-	Globals    *Globals    `hcl:"globals,block"`
-	Define     *Define     `hcl:"define,block"`
-	Template   *Template   `hcl:"template,block"`
+	Terradude  *Terradude    `hcl:"terradude,block"`
+	Terraform  *Terraform    `hcl:"terraform,block"`
+	Dependency []*Dependency `hcl:"dependency,block"`
+	Backend    *Backend      `hcl:"backend,block"`
+	Provider   []*Provider   `hcl:"provider,block"`
+	Globals    *Globals      `hcl:"globals,block"`
+  Remain 		 hcl.Body      `hcl:",remain"`
+	// Define     *Define     `hcl:"define,block"`
+	// Template   *Template   `hcl:"template,block"`
 }
 
 type Terradude struct {
-	Version string `hcl:"version"`
-  Remain hcl.Body `hcl:",remain"`
+	Version string   `hcl:"version"`
+  Remain  hcl.Body `hcl:",remain"`
 }
 
 type Terraform struct {
@@ -27,13 +29,18 @@ type Terraform struct {
 }
 
 type Dependency struct {
-	Name string `hcl:"name,label"`
-	Path string `hcl:"path,attr"`
+	Name   string   `hcl:"name,label"`
+	Path   string   `hcl:"path,attr"`
   Remain hcl.Body `hcl:",remain"`
 }
 
 type Backend struct {
-	Name string `hcl:"name,label"`
+	Name string   `hcl:"name,label"`
+  Body hcl.Body `hcl:",remain"`
+}
+
+type Provider struct {
+	Name string   `hcl:"name,label"`
   Body hcl.Body `hcl:",remain"`
 }
 
@@ -41,12 +48,12 @@ type Globals struct {
   Body hcl.Body `hcl:",remain"`
 }
 
-type Define struct {
-	Name string `hcl:"name,label"`
-  Body hcl.Body `hcl:",remain"`
-}
-
-type Template struct {
-	Name string `hcl:"name,label"`
-  Body hcl.Body `hcl:",remain"`
-}
+// type Define struct {
+// 	Name string `hcl:"name,label"`
+//   Body hcl.Body `hcl:",remain"`
+// }
+//
+// type Template struct {
+// 	Name string `hcl:"name,label"`
+//   Body hcl.Body `hcl:",remain"`
+// }
--- live/stage/aws/terradude.hcl
+++ live/stage/aws/terradude.hcl
@@ -1,7 +1,10 @@
-template "provider-aws" {
-  version = "~> 0.29"
-
+provider "aws" {
+  version             = "~> 0.29"
   allowed_account_ids = [ "0123456789" ]
+  region              = global.aws_region
+}
+
+provider "random" {
 }
 
 globals {
