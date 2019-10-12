889386bfdfb0d64cc41a76aa06d34239f5994340
Marius Tolzmann <m@rius.berlin>
2019-10-12 21:14:02 +0200

    introduces define/template blocks instead of files blocks

--- config/config.go
+++ config/config.go
@@ -4,17 +4,33 @@ const DefaultConfigPath = "terradude.hcl"
 
 // represents a parsed config
 type Config struct {
-	Terradude *Terradude `hcl:"terradude,block"`
-	Backend   *Backend   `hcl:"backend,block"`
-	Globals   *Globals   `hcl:"globals,block"`
+	Terradude  *Terradude  `hcl:"terradude,block"`
+	Terraform  *Terraform  `hcl:"terraform,block"`
+	Dependency *Dependency `hcl:"dependency,block"`
+	Backend    *Backend    `hcl:"backend,block"`
+	Globals    *Globals    `hcl:"globals,block"`
+	Define     *Define     `hcl:"define,block"`
+	Template   *Template   `hcl:"template,block"`
 }
 
 type Terradude struct {
 	Version string `hcl:"version"`
 }
 
+type Terraform struct {
+}
+
+type Dependency struct {
+}
+
 type Backend struct {
 }
 
 type Globals struct {
+}
+
+type Define struct {
+}
+
+type Template struct {
 }
