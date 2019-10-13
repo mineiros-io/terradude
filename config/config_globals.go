c852f534d225888270a44510d96653e2d79e51d9
Marius Tolzmann <m@rius.berlin>
2019-10-13 07:00:56 +0200

    write out first poc of s3 backend

--- config/config_globals.go
+++ config/config_globals.go
@@ -1,43 +1,51 @@
 package config
 
 import (
+	"github.com/zclconf/go-cty/cty/gocty"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/hashicorp/hcl/v2"
 )
+
 
 type Global struct {
 	Name string
 	Value cty.Value
 }
 
-func DecodeGlobalValues(hclconfigs []*Config) ([]*Global, hcl.Diagnostics) {
-	var globals []*Global
+func DecodeGlobalCty(hclconfigs []*Config) (*cty.Value, hcl.Diagnostics) {
 	var diags   hcl.Diagnostics
+	globals := map[string]cty.Value{}
 
-  for _, hclconfig := range hclconfigs {
+	for _, hclconfig := range hclconfigs {
 		if hclconfig.Globals != nil {
 			attrs, diag := hclconfig.Globals.Body.JustAttributes()
 			if diag.HasErrors() {
 				return nil, diags
 			}
 			diags = append(diags, diag...)
-			attrloop: for _, attr := range attrs {
-				for _, global := range globals {
-					if (global.Name == attr.Name) {
-						continue attrloop
-					}
+			for _, attr := range attrs {
+				if _, ok := globals[attr.Name] ; ok {
+					continue
 				}
 				value, diag := attr.Expr.Value(nil)
 				diags = append(diags, diag...)
 				if diag.HasErrors() {
 					return nil, diags
 				}
-				globals = append(globals, &Global{
-					Name: attr.Name,
-					Value: value,
-				})
+				globals[attr.Name] = value
 			}
 		}
 	}
-	return globals, diags
+
+	ctyTypes := map[string]cty.Type{}
+	for key, value := range globals {
+		ctyTypes[key] = value.Type()
+	}
+	ctyObject := cty.Object(ctyTypes)
+	ctyGlobals, err := gocty.ToCtyValue(globals, ctyObject)
+	if err != nil {
+		return nil, err.(hcl.Diagnostics)
+	}
+
+	return &ctyGlobals, nil
 }
