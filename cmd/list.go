346bbc4a947ae126a2cef1a17b6a231c1d399ec8
Soren Martius <soeren.martius@gmail.com>
2019-10-18 13:43:57 +0100

    adding list command

--- cmd/list.go
+++ cmd/list.go
@@ -2,11 +2,9 @@ package cmd
 
 import (
 	"github.com/mineiros-io/terradude/config"
-	"github.com/mineiros-io/terradude/dude"
 	"github.com/mineiros-io/terradude/util"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var listCmd = &cobra.Command{
@@ -14,17 +12,11 @@ var listCmd = &cobra.Command{
 	Short: "List all found terraform modules",
 	Long:  `Searches the given path backwards recursively for terradude.hcl files`,
 	Run: func(cmd *cobra.Command, args []string) {
-
-		leafs, _ := util.FindLeafFiles(config.DefaultConfigFileBaseName, os.Args[1:], nil)
+		leafs, _ := util.FindLeafFiles(config.DefaultConfigFileBaseName, args[0:], nil)
 
 		for _, leaf := range leafs {
 			log.Debug().Msgf("found leaf in %s", leaf)
 		}
-
-		for _, leaf := range leafs {
-			dude.RunFmt(leaf)
-		}
-
 	},
 }
 
