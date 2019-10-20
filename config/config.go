7171a6e4fc2eea99a856f2d9a468e752828f2c17
Marius Tolzmann <m@rius.berlin>
2019-10-20 17:32:57 +0200

    cleanup config description

--- config/config.go
+++ config/config.go
@@ -34,55 +34,21 @@ type Module struct {
 type Dependency struct {
 	Name   string   `hcl:"name,label"`
 	Path   string   `hcl:"path,attr"`
-  Remain hcl.Body `hcl:",remain"`
+	Body   hcl.Body `hcl:",remain"`
 }
 
 type Backend struct {
 	Name   string   `hcl:"name,label"`
-  Body   hcl.Body `hcl:",remain"`
+	Body   hcl.Body `hcl:",remain"`
 }
-
-type BackendS3 struct {
-	Bucket             string   `hcl:"bucket"`
-	Key                string   `hcl:"key"`
-	Region             string   `hcl:"region"`
-	dynamodb_endpoint  *string  `hcl:"dynamodb_endpoint"`
-	// endpoint
-	// iam_endpoint
-	// sts_endpoint
-	// encrypt
-	// acl
-	// access_key
-	// secret_key
-	// kms_key_id
-	// lock_table
-	// dynamodb_table
-	// profile
-	// shared_credentials_file
-	// token
-	// skip_credentials_validation
-	// skip_get_ec2_platforms
-	// skip_region_validation
-	// skip_requesting_account_id
-	// skip_metadata_api_check
-	// role_arnrole_arn
-	// session_name
-	// external_id
-	// assume_role_policy
-	// workspace_key_prefix
-	// force_path_style
-	// max_retries
-	//
-	//
-	//
-}
-
 
 type Provider struct {
-	Name string   `hcl:"name,label"`
-  Body hcl.Body `hcl:",remain"`
+	Name    string   `hcl:"name,label"`
+	Version *string  `hcl:"version,attr"`
+	Alias   *string  `hcl:"alias,attr"`
+	Body    hcl.Body `hcl:",remain"`
 }
 
 type Globals struct {
-  Body hcl.Body `hcl:",remain"`
+	Body hcl.Body `hcl:",remain"`
 }
